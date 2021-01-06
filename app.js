2c54bd41809b10755f6a8a155d63e863ae60d7a5
Shaqif-Ahmed <71043863+Shaqif-Ahmed@users.noreply.github.com>
2021-01-06 07:21:13 -0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const list = document.getElementsByClassName("listbx");
-for (i = 0; i < list.length; i++) {
-  list[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-  });
-}
